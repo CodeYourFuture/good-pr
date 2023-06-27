423bfde91d5b1190545ae69bbc3e06043d15ead8
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-06-27 20:01:42 +0100

    create a TeamCard

---
+++ components/TeamCard.js
@@ -0,0 +1,8 @@
+export default function TeamCard() {
+  return (
+    <div className="bg-[#FFFFFF] p-3 w-80">
+      <div className="bg-[#F6F8FA]">11111111</div>
+      <div className="bg-[#FFFFFF]">22222222</div>
+    </div>
+  );
+}
