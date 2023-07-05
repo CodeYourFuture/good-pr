c565024c688697bb1bcf9a56e5fe61bb712090db
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-07-05 23:51:38 +0100

    Add GroupsSidebar to the teams page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import GroupsPage from "@components/GroupsPage";
-import Sidebar from "@components/Sidebar";
+import GroupsSidebar from "@components/GroupsSidebar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen justify-between p-0 text-[white] ">
       <div className="flex border-neutral-800  overflow-hidden rounded-lg">
-        <Sidebar />
+        <GroupsSidebar />
         <div className="flex-1 w-screen bg-[#070e0e] pl-[5%] pr-[5%]">
           <div class="p-4 py-12">
             <h1 className="text-[30px] font-bold">Teams</h1>
