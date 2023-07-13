21178086593c407cf097432eec509ccbb982819d
Mo Mahboobian <momahboobian@gmail.com>
2023-07-13 13:24:02 +0100

    Change the see details

--- components/TasksActivity.js
+++ components/TasksActivity.js
@@ -78,7 +78,7 @@ export default function TaskActivity({ allIssues, repo }) {
                         target="_blank"
                         className="py-3 px-6  bg-[#2b3234] border-none hover:text-gray-500 duration-150 hover:bg-gray-50 border rounded-lg"
                       >
-                        See Details
+                        See Details...
                       </a>
                     </td>
                   </tr>
